3b78b33949db1df6db9333c6dfa8fd3abb800d40
Eers <sreeaadhi07@gmail.com>
2024-01-31 00:19:56 +0530

    added twilio functions

---
+++ lib/twilio_functions.py
@@ -0,0 +1,94 @@
+from dotenv import load_dotenv
+from twilio.twiml.voice_response import VoiceResponse, Start
+from twilio.rest import Client
+from fastapi import WebSocket, WebSocketDisconnect, HTTPException
+import os
+import audioop
+import base64
+import json
+
+from audio_buffer import AudioBuffer
+from asr import transcribe_buffer
+
+load_dotenv()
+
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+
+twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+TWILIO_PHONE_NUMBER = twilio_client.incoming_phone_numbers.list()[0]
+
+
+def update_phone(public_url: str, phone_number: str) -> None:
+    phone = list(
+        twilio_client.incoming_phone_numbers.list(phone_number=phone_number))
+    if len(phone) == 0:
+        raise Exception("No phone numbers found.")
+
+    phone.update(
+        voice_url=f"{public_url}/{phone_number}/call"
+    )
+
+
+async def call_accept(public_url: str, phone_number: str) -> VoiceResponse:
+    response = VoiceResponse()
+    start = Start()
+    start.stream(
+        url=f'wss://{public_url.split("//")[1]}/{phone_number}/stream')
+    response.append(start)
+    response.say('Hello. How may I help you today?')
+    response.pause(length=60)
+    return response
+
+
+async def voice_response(transcription_text: str, call_sid: str, twilio_client: Client) -> None:
+    call_session = twilio_client.calls(call_sid)
+    call_session.update(
+        twiml=f'<Response><Say>{transcription_text}</Say><Pause length="60"/></Response>'
+    )
+
+
+async def call_stream(websocket: WebSocket) -> None:
+    audio_buffer = AudioBuffer()
+    buffer_threshold = 16000  # Initial buffer threshold
+    call_sid = None
+
+    await websocket.accept()
+
+    try:
+        while True:
+            message = await websocket.receive_text()
+            packet = json.loads(message)
+
+            if packet['event'] == 'start':
+                print('Media stream started!')
+                call_sid = packet['start']['callSid']
+            elif packet['event'] == 'stop':
+                print('Media stream stopped!')
+
+            if packet['event'] == 'media':
+                chunk = base64.b64decode(packet['media']['payload'])
+                # Convert audio data from ulaw to linear PCM
+                audio_data = audioop.ulaw2lin(chunk, 2)
+                # Convert audio data from 8kHz to 16kHz for Whisper
+                audio_data = audioop.ratecv(
+                    audio_data, 2, 1, 8000, 16000, None)[0]
+                # Compute audio energy
+                audio_energy = audioop.rms(audio_data, 2)
+                energy_threshold = 700
+
+                if audio_energy >= energy_threshold:
+                    audio_buffer.append(audio_data)
+
+                if len(audio_buffer) >= buffer_threshold:
+                    transcription_result = transcribe_buffer(audio_buffer)
+                    print("Transcription:", transcription_result)
+                    # Clear the buffer after transcription
+                    audio_buffer.clear()
+                    print(f"Call SID: {call_sid}")
+                    await voice_response(transcription_result, call_sid)
+
+    except WebSocketDisconnect:
+        print("WebSocket disconnected")
+    except HTTPException as e:
+        print(f"HTTP Exception: {e}")
