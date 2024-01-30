874d1d4253bac998448fa20196766a0ea5914dd2
Eers <sreeaadhi07@gmail.com>
2024-01-30 23:54:25 +0530

    added asr module

---
+++ lib/asr.py
@@ -0,0 +1,31 @@
+from audio_buffer import AudioBuffer
+import os
+import wave
+
+from faster_whisper import WhisperModel
+
+# Initialize faster_whisper model
+model_size = "small"
+STTmodel = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+
+
+def transcribe_buffer(audio_buffer: AudioBuffer) -> str:
+    temp_audio_file = 'temp_audio.wav'
+    temp_audio_path = os.path.join(os.getcwd(), temp_audio_file)
+
+    with wave.open(temp_audio_path, 'wb') as wf:
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(16000)
+        wf.writeframes(audio_buffer.read())
+
+    # Pass the audio file to Whisper for transcription
+    with open(temp_audio_path, 'rb') as audio_file:
+        segments, info = STTmodel.transcribe(
+            audio_file, beam_size=5, task="transcribe")
+
+        transcription = ''
+        for segment in segments:
+            transcription += segment.text
+
+    return transcription
