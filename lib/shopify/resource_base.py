a9a6d9f0324159b84f714811d1e21aaf052ae1ee
Eers <sreeaadhi07@gmail.com>
2024-02-06 09:22:22 +0530

    Shopify Client

---
+++ lib/shopify/resource_base.py
@@ -0,0 +1,59 @@
+import requests
+
+
+class ShopifyResource(requests.Session):
+    def __init__(self, token, store, api_version="2024-01") -> None:
+        super().__init__()
+        self.token = token
+        self.store = store
+        self.api_version = api_version
+        self.baseurl = f"https://{self.store}.myshopify.com/admin/api/{self.api_version}"
+
+        self.headers.update({
+            "X-Shopify-Access-Token": self.token,
+            "Content-Type": "application/json"
+        })
+
+    def request(self, method, url, *args, **kwargs):
+        full_url = f"{self.baseurl}{url}"
+        return super().request(method, full_url, *args, **kwargs)
+
+
+class ShopifyResourceBase:
+    def __init__(self, resource: ShopifyResource, resource_name) -> None:
+        self.resource = resource
+        self.resource_name = resource_name
+
+    def get(self, url):
+        return self.resource.get(url)
+
+    def post(self, url, data):
+        return self.resource.post(url, json=data)
+
+    def put(self, url, data):
+        return self.resource.put(url, json=data)
+
+    def delete(self, url):
+        return self.resource.delete(url)
+
+
+class Orders(ShopifyResourceBase):
+    def __init__(self, resource: ShopifyResource) -> None:
+        super().__init__(resource, "orders")
+
+    def get_orders(self):
+        return self.get("/orders.json")
+
+    def get_order(self, order_id):
+        return self.get(f"/orders/{order_id}.json")
+
+    def create_order(self, data):
+        return self.post("/orders.json", data)
+
+    def update_order(self, order_id, data):
+        return self.put(f"/orders/{order_id}.json", data)
+
+    def delete_order(self, order_id):
+        return self.delete(f"/orders/{order_id}.json")
+
+# TODO: Add Products class
---
+++ lib/shopify/shopify_client.py
@@ -0,0 +1,24 @@
+from resource_base import ShopifyResource, Orders
+from rich import print
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class ShopifyClient:
+    def __init__(self, resource: ShopifyResource) -> None:
+        self.resource = resource
+        self.Orders = Orders(resource)
+        # TODO: Add self.Products = Products(resource)
+
+
+if __name__ == "__main__":
+    resource = ShopifyResource(token=os.getenv(
+        "SHOPIFY_API_KEY"), store="b59bb6-2")
+    client = ShopifyClient(resource)
+    orders = client.Orders.get_orders()
+
+    if orders.status_code == 200:
+        print("HTTP/1.1 200 OK | API Works!")
+    else:
+        print(f"HTTP/1.1 {orders.status_code} {orders.text} | API Error!")
