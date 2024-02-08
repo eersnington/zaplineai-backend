da38e7336536345e25bd0230a244af8deb23b6e2
Eers <sreeaadhi07@gmail.com>
2024-02-08 21:43:33 +0530

    Update phone.py

--- routers/phone.py
+++ routers/phone.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, HTTPException
+import sqlite3
+from fastapi import APIRouter
 from pydantic import BaseModel
 
 from lib.custom_exception import CustomException
 from lib.twilio_functions import get_new_numbers, get_available_numbers, get_unused_phone_number
 from lib.db import db
+from lib.auth import check_user
 
 router = APIRouter(prefix="/phone")
 
@@ -13,6 +15,9 @@ class PhoneForm(BaseModel):
 
 
 async def assign_number(user_id: str, phone_number: str):
+    """
+        Assigns a phone number to a user in the database.
+    """
     try:
         await db.bot.create({"userId": user_id, "phone_no": phone_number, "refund_accept": "False"})
         return {"message": "Phone number assigned."}
@@ -20,24 +25,34 @@ async def assign_number(user_id: str, phone_number: str):
         raise CustomException("Error assigning phone number.", 500)
 
 
-async def check_user(form: PhoneForm):
-    user = await db.user.find_first(where={"id": form.user_id})
-    if user is None:
-        raise CustomException("User not found.", 401)
-
-
 async def check_user_number(form: PhoneForm):
+    """
+        Checks if a user already has a phone number.
+    """
     user = await db.bot.find_first(where={"userId": form.user_id})
     if user is not None:
         raise CustomException("User already has a phone number.", 400)
 
 
+def initiatlize_user_metrics(user_id: str):
+    """
+        Initializes user metrics in the database.
+    """
+    sql = '''INSERT INTO user_metrics (user_id, calls, automated_calls, transferred_calls, abandoned_calls)
+             VALUES (?, ?, ?, ?, ?)'''
+    data = (user_id, 0, 0, 0, 0)
+
+    conn = sqlite3.connect('user_metrics.db')
+    c = conn.cursor()
+
+    with conn:
+        c.execute(sql, data)
+        conn.commit()
+
+
 @router.get("/new")
 async def list_phones(form: PhoneForm):
-    try:
-        await check_user(form)
-    except CustomException as e:
-        raise HTTPException(status_code=e.status_code, detail=str(e))
+    await check_user(form.user_id)
 
     phone_numbers = get_new_numbers()
     return {"message": "Available phone numbers.", "phone_numbers": phone_numbers}
@@ -45,10 +60,7 @@ async def list_phones(form: PhoneForm):
 
 @router.get("/available")
 async def available_phones(form: PhoneForm):
-    try:
-        await check_user(form)
-    except CustomException as e:
-        raise HTTPException(status_code=e.status_code, detail=str(e))
+    await check_user(form.user_id)
 
     phone_numbers = await get_available_numbers(get_first=False)
     return {"message": "Available phone numbers.", "phone_numbers": phone_numbers}
@@ -56,16 +68,14 @@ async def available_phones(form: PhoneForm):
 
 @router.post("/buy")
 async def buy_phone(form: PhoneForm):
-    try:
-        await check_user(form)
-        await check_user_number(form)
-    except CustomException as e:
-        raise HTTPException(status_code=e.status_code, detail=str(e))
+    await check_user(form.user_id)
+    await check_user_number(form)
 
     phone_number = await get_unused_phone_number()
     try:
         await assign_number(form.user_id, phone_number)
-    except CustomException as e:
-        raise HTTPException(status_code=e.status_code, detail=str(e))
+        initiatlize_user_metrics(form.user_id)
+    except Exception as e:
+        raise CustomException(e.__str__, 500)
 
     return {"message": "Phone number purchased.", "phone_number": phone_number}
