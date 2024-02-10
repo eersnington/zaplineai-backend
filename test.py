0496d0ad0afd6fc06e15f5e0d2f6058e18861828
Eers <sreeaadhi07@gmail.com>
2024-02-10 18:02:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
-from transformers import BertForSequenceClassification, BertTokenizer, TextClassificationPipeline
-
+from lib.llm_model import BERTClassifier
 import time
 # llm_model = LLMModel()
 
 # chat = LLMChat(llm_model)
-
 
 # while True:
 #     inp = input("Enter your prompt: ")
 #     start = time.time()
 #     output = chat.generate_response(inp)
 #     print(f"{output}\n\nTime taken: {time.time() - start}s")
-model_path = "Sreenington/BERT-Ecommerce-Classification"
-tokenizer = BertTokenizer.from_pretrained(model_path)
-model = BertForSequenceClassification.from_pretrained(model_path)
-pipeline = TextClassificationPipeline(model=model, tokenizer=tokenizer)
+
+bert = BERTClassifier()
 
 while True:
     inp = input("Enter your prompt: ")
     start = time.time()
-    output = pipeline(inp)
+    output = bert.classify(inp)
     print(f"{output}\n\nTime taken: {time.time() - start}s")
