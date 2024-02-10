63c41ef7d0e93fcb302c2803612635da0efc60c4
Eers <sreeaadhi07@gmail.com>
2024-02-10 16:39:57 +0530

    Update llm_model.py

--- lib/llm_model.py
+++ lib/llm_model.py
@@ -7,16 +7,28 @@ import functools
 
 
 @functools.cache
-def get_llm_model() -> LLM:
-    logging.info(f"Loading Llama LLM")
-    llm = LLM(model="TheBloke/Llama-2-7B-chat-AWQ", quantization="awq")
+def get_llm_model(model: str, quantization: str | None) -> LLM:
+    logging.info(f"Loading LLM model: {model}")
+    if quantization is None:
+        llm = LLM(model=model)
+    else:
+        llm = LLM(model=model, quantization="awq")
 
     return llm
 
 
+class BERTClassifier:
+    def __init__(self):
+        self.llm = get_llm_model("Sreenington/BERT-Ecommerce-Classification")
+
+    def classify(self, text: str) -> str:
+        outputs = self.llm.generate(text, use_tqdm=False)
+        return outputs[0].outputs[0].text
+
+
 class LLMModel:
     def __init__(self):
-        self.llm = get_llm_model()
+        self.llm = get_llm_model("TheBloke/Llama-2-7B-chat-AWQ", "awq")
 
     def generate_text(self, prompt: str, temperature: 0.8, max_tokens=100) -> str:
         sampling_params = SamplingParams(
