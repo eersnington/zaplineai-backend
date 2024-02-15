94ce2fe43ab4a502134fe3406ae8512499961833
Sree Narayanan <sreeaadhi07@gmail.com>
2024-02-15 06:50:27 +0530

    Update audio_buffer.py

--- lib/audio_buffer.py
+++ lib/audio_buffer.py
@@ -1,3 +1,12 @@
+import io
+import os
+import tempfile
+import queue
+import logging
+
+from pydub import AudioSegment
+import speech_recognition as sr
+
 class AudioBuffer:
     """
     The AudioBuffer class is used to store audio data.
@@ -20,3 +29,53 @@ class AudioBuffer:
 
     def __str__(self):
         return self.buffer
+    
+
+class _TwilioSource(sr.AudioSource):
+    def __init__(self, stream):
+        self.stream = stream
+        self.CHUNK = 1024
+        self.SAMPLE_RATE = 8000
+        self.SAMPLE_WIDTH = 2
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+
+class _QueueStream:
+    def __init__(self):
+        self.q = queue.Queue(maxsize=-1)
+
+    def read(self, chunk: int) -> bytes:
+        return self.q.get()
+
+    def write(self, chunk: bytes):
+        self.q.put(chunk)
+
+
+class WhisperTwilioStream:
+    def __init__(self, whisper_model):
+        self.audio_model = whisper_model
+        self.recognizer = sr.Recognizer()
+        self.recognizer.energy_threshold = 300
+        self.recognizer.pause_threshold = 2.5
+        self.recognizer.dynamic_energy_threshold = False
+        self.stream = None
+
+    def get_transcription(self) -> str:
+        self.stream = _QueueStream()
+        with _TwilioSource(self.stream) as source:
+            logging.info("Waiting for twilio caller...")
+            with tempfile.TemporaryDirectory() as tmp:
+                tmp_path = os.path.join(tmp, "mic.wav")
+                audio = self.recognizer.listen(source)
+                data = io.BytesIO(audio.get_wav_data())
+                audio_clip = AudioSegment.from_file(data)
+                audio_clip.export(tmp_path, format="wav")
+                result = self.audio_model.transcribe(tmp_path, language="english")
+        predicted_text = result["text"]
+        self.stream = None
+        return predicted_text
