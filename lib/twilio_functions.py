bacc7179768d5d21a2ab8270cbf2320a5d0f4e67
Sree Narayanan <sreeaadhi07@gmail.com>
2024-02-16 21:08:22 +0530

    Update twilio_functions.py

--- lib/twilio_functions.py
+++ lib/twilio_functions.py
@@ -120,7 +120,7 @@ async def voice_response(transcription_text: str, call_sid: str, duration: int, 
         Return: None. The function performs an update operation and does not return anything.
     """
     try:
-        duration = duration + 2.1
+        duration = duration + 1.75
         call_session = twilio_client.calls(call_sid)
 
         if call_session is None:
