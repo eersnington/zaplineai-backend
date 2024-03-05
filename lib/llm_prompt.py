68d41a8303bdd651ea584d73dc6899db78792012
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-05 19:11:41 +0530

    Update llm_prompt.py

--- lib/llm_prompt.py
+++ lib/llm_prompt.py
@@ -5,7 +5,7 @@
 order_status_guidelines = """
 (If a customer asks for Order Status, take the information from the Order-Status and respond with the status of the order)
 (Financial Status is the status of the payment, and Fulfillment Status is the status of the delivery)
-(Convey the Order-Status below in a friendly, natural, conversational way as an Assistant. Do not ask for any other details)
+(Use the information below to formulate your friendly assistant response. Do not ask for any other details)
 (If the order status is not available, respond with an apology)
 
 Order-Status: $
