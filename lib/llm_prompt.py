8e0763f39e2d6bdd4f7f973aa672c743231a3e3e
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-10 01:35:00 +0530

    Update llm_prompt.py

--- lib/llm_prompt.py
+++ lib/llm_prompt.py
@@ -2,54 +2,9 @@
     This file contains the prompt for the LLM model in different prompt formats.
 """
 
-order_status_guidelines = """
-(If a customer asks for Order Status, take the information from the Order-Status and reply in the format given below)
-(Financial Status is the status of the payment, and Fulfillment Status is the status of the delivery)
-(Fulfillment Staus Descriptions are as follows: 
-"Fulfilled" means the order has been delivered,
-"Unfulfilled" means the team is working on shipping the order,
-"Partially Fulfilled" means part of the order has been delivered,
-"Scheduled" means the order is scheduled for delivery,
-"On hold" means the order is on hold,
-)
-```
-Order-Status (From server): $
-```
 
-Follow this format of your response:
-"Sure! Thank you for checking in on your order. According to our records, <<explain the order status here>>.  If you have any further questions or concerns, please don't hesitate to ask."
-"""
-
-returns_guidelines_step1 = """
-(If a customer asks for Return, please ask them for a reason for the return)
-
-Follow this format of your response (Do not add anything extra):
-"If you'd like to initiate a return, please provide a reason for the return. I'll then initiate a return ticket for you, and our team will contact you soon to assist further."
-"""
-
-returns_guidelines_step2 = """
-(Customer has already asked for a refund and stated their reason. Now say the following to the customer)
-
-Follow this format of your response (Do not add anything extra):
-"Thank you for providing the reason for your return. I've initiated a return ticket for you, and our team will contact you soon to assist further. Is there anything else I can help you with?"
-"""
-
-refund_guidelines_step1 = """
-(If a customer asks for a refund, please ask them for a reason for the refund)
-
-Follow this format of your response (Do not add anything extra):
-"If you'd like to request a refund, please provide a reason for the refund. I'll then initiate a refund ticket for you, and our team will contact you soon to assist further."
-"""
-
-refund_guidelines_step2 = """
-(Customer has already asked for a refund and stated their reason. Now say the following to the customer)
-
-Follow this format of your response (Do not add anything extra):
-"Thank you for providing the reason for your refund. I've initiated a refund ticket for you, and our team will contact you soon to assist further. Is there anything else I can help you with?"
-"""
-
-sales_or_transfer_guidelines = """
-(If a customer asks for Sales or Transfer, tell to the customer that you (the AI assistant) will immediately transfer the call to a live respresentative right now)
+sales_guidelines = """
+(If a customer asks for Sales or a Sales related query, tell that the call will be transferred a live respresentative)
 
 Follow this format for your response (Do not add anything extra):
 "Sure thing! I can transfer your call right away. Please hold on for a moment while I connect you."
@@ -57,29 +12,28 @@ Follow this format for your response (Do not add anything extra):
 
 def get_guidelines(call_intent: str, data: str) -> str:
     if call_intent in ["Sales"]:
-        return sales_or_transfer_guidelines
+        return sales_guidelines
     elif call_intent == "Product Info":
         return "Provide the customer with the information they are looking for."
     elif call_intent == "General Inquiry":
-        return ""
+        return "Answer Generally."
     else:
         return ""
 
 
-def llama_prompt(prompt: str, call_intent: str, data: str, chat_history: list | None) -> str:
+def llama_prompt(prompt: str, call_intent: str, data: str, chat_history: list) -> str:
 
     system_prompt = f"""[INST] <<SYS>>
-You are friendly AI assistant for a shopify store, and you are having a continuing conversation with a customer with their queries.
-(Do not greet them with a hello or thank you, as the call is already connected and the conversation is ongoing)
-
-Call Intent (based on Classification Model):
-```
-# It is the intent of the call, based on the classification model
-Call Intent: {call_intent}
-```
+You are friendly and humanly AI assistant for a shopify store, and you are having a continuing conversation with a customer with their queries.
 
 Guidelines for the call:
+(Do not greet them with a hello or thank you, as the call is already connected and the conversation is ongoing)
 {get_guidelines(call_intent, data)}
+
+Chat History (Use this to provide context):
+```
+{(len(chat_history) == 0) and "No context available... (This is the begining of the conversation)" or chat_history}
+```
 
 Provide a concise response, considering the context.
 Keep it direct and to the point.
