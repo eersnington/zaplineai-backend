a683c3e364096e2bc90d14a4e52e11717f36c8d7
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-10 22:55:46 +0530

    db updates

--- lib/db.py
+++ lib/db.py
@@ -15,11 +15,12 @@ with conn:
                      transferred_calls INTEGER,
                      abandoned_calls INTEGER,
                      call_type TEXT DEFAULT "automated",
+                     call_intent TEXT,
                      timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                      )''')
 
 
-def track_metrics(user_id: str, call_type: str) -> None:
+def track_metrics(user_id: str, call_type: str, call_intent: str) -> None:
     """
         Tracks the metrics of a user's call.
 
@@ -29,17 +30,17 @@ def track_metrics(user_id: str, call_type: str) -> None:
 
         Return: None. The function performs an insert operation and does not return anything.
     """
-    sql = '''INSERT INTO user_metrics (user_id, calls, automated_calls, transferred_calls, abandoned_calls, call_type)
-                VALUES (?, ?, ?, ?, ?, ?)'''
+    sql = '''INSERT INTO user_metrics (user_id, calls, automated_calls, transferred_calls, abandoned_calls, call_type, call_intent)
+                VALUES (?, ?, ?, ?, ?, ?, ?)'''
 
     if call_type == "automated":
-        data = (user_id, 1, 1, 0, 0, "automated")
+        data = (user_id, 1, 1, 0, 0, "automated", call_intent)
         c.execute(sql, data)
     elif call_type == "transferred":
-        data = (user_id, 1, 0, 1, 0, "transferred")
+        data = (user_id, 1, 0, 1, 0, "transferred", call_intent)
         c.execute(sql, data)
     elif call_type == "abandoned":
-        data(user_id, 1, 0, 0, 1, "abandoned")
+        data(user_id, 1, 0, 0, 1, "abandoned", "Neither")
         c.execute(sql, data)
 
     conn.commit()
