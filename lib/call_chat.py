99b8d6fca0bd3ed8ddbf4af42e692d9d7c5b298f
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-10 09:49:07 +0530

    Update call_chat.py

--- lib/call_chat.py
+++ lib/call_chat.py
@@ -83,7 +83,7 @@ class CallChatSession:
         if self.order_id is None:
             return "I couldn't find any latest orders for you. If you think this is a mistake, please call again later."
         
-        print(f"Order ID: {self.order_id}")
+        print(f"-----------Order ID-----------------: {self.order_id}")
         note_text = f"Return initiated by customer through call. Reason: {self.return_refund_reason}"
         self.client.Orders.update_order(self.order_id, {"order": {"note": note_text}})
         return get_intent_response("Returns Step2")
@@ -99,7 +99,9 @@ class CallChatSession:
         if self.order_id is None:
             return "I couldn't find any latest orders for you. If you think this is a mistake, please call again later."
         
-        self.client.Orders.update_order(self.order_id, {"order": {"note": f"Refund initiated by customer through call. Reason: {self.return_refund_reason}"}})
+        print(f"Order ID: {self.order_id}")
+        note_text = f"Refund initiated by customer through call. Reason: {self.return_refund_reason}"
+        self.client.Orders.update_order(self.order_id, {"order": {"note": note_text}})
         return get_intent_response("Refund Step2")
     
 
@@ -111,7 +113,8 @@ class CallChatSession:
         if reason:
             self.return_refund_reason = reason
             self.return_order = False
-            return self.initiate_return()
+            response = self.initiate_return()
+            return response
             
 
     def refund_process(self, reason) -> str:
