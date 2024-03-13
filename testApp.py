dcbceff38c6543cc4ed95b12ec86413eea7e5ba2
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-13 19:52:44 +0530

    Update testApp.py

--- testApp.py
+++ testApp.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import pipeline
 from transformers.utils import is_flash_attn_2_available
-
+import time
 pipe = pipeline(
     "automatic-speech-recognition",
     model="openai/whisper-large-v3", # select checkpoint from https://huggingface.co/openai/whisper-large-v3#model-details
@@ -10,6 +10,7 @@ pipe = pipeline(
     model_kwargs={"attn_implementation": "flash_attention_2"} if is_flash_attn_2_available() else {"attn_implementation": "sdpa"},
 )
 
+start = time.time()
 outputs = pipe(
     "audio.wav",
     chunk_length_s=30,
@@ -18,3 +19,4 @@ outputs = pipe(
 )
 
 print(outputs)
+print(f"Time taken: {time.time() - start} seconds")
