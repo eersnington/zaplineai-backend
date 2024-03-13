d6c9c76c2869982a4e87500e14a3c8c672fa0e32
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-13 20:36:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from pyngrok import ngrok
 from lib.db import db
 from lib.auth import check_user
 from lib.custom_exception import CustomException
-#from lib.twilio_functions import call_accept, call_stream, update_phone
+from lib.twilio_functions import call_accept, call_stream, update_phone
 from routers.phone import router as phone_router
 from routers.metrics import router as metrics_router
 logging.getLogger().setLevel(logging.INFO)
