8515f1b9068f0930ed5f69287624a371e06d3b75
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-13 21:03:21 +0530

    Update llm_model.py

--- lib/llm_model.py
+++ lib/llm_model.py
@@ -61,7 +61,7 @@ class LLMModel:
     def __init__(self):
         self.llm = get_vllm_model("TheBloke/Llama-2-7B-chat-AWQ", "awq")
 
-    def generate_text(self, prompt: str, temperature: 0.8, max_tokens=100) -> str:
+    def generate_text(self, prompt: str, temperature=0.8, max_tokens=100) -> str:
         sampling_params = SamplingParams(
             temperature=temperature, max_tokens=max_tokens)
 
