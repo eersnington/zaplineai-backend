c86fbcb7e340842e9a12bc9e3a158e70f25df8b0
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-13 21:31:05 +0530

    Update llm_perf.py

--- llm_perf.py
+++ llm_perf.py
@@ -6,6 +6,8 @@ llm_model = LLMModel()
 while True:
     # Generate text using the LLM model
     text = input("Enter your message: ")
+    if text == "exit":
+        break
     start = time.time()
     llm_model.generate_text(text)
     end = time.time()
