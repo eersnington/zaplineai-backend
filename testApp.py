fa5dc0ba37783cadc0e2420a15d2911fdd9201d2
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-13 20:05:18 +0530

    Update testApp.py

--- testApp.py
+++ testApp.py
@@ -5,7 +5,7 @@ import time
 pipe = pipeline(
     "automatic-speech-recognition",
     model="openai/whisper-large-v3", # select checkpoint from https://huggingface.co/openai/whisper-large-v3#model-details
-    torch_dtype=torch.float16,
+    torch_dtype=torch.int8,
     device="cuda:0", # or mps for Mac devices
     model_kwargs={"attn_implementation": "flash_attention_2"} if is_flash_attn_2_available() else {"attn_implementation": "sdpa"},
 )
@@ -15,7 +15,7 @@ outputs = pipe(
     "audio.wav",
     chunk_length_s=30,
     batch_size=24,
-    return_timestamps=True,
+    return_timestamps=False,
 )
 
 print(outputs["text"])
