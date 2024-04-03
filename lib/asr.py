78d89bfa6786eb874a057a29084c031af72b3a9a
Sree Narayanan <sreeaadhi07@gmail.com>
2024-04-03 07:03:36 +0530

    Update asr.py

--- lib/asr.py
+++ lib/asr.py
@@ -42,7 +42,7 @@ else:
 
 
 recognizer = sr.Recognizer()
-recognizer.energy_threshold = 400  
+recognizer.energy_threshold = 600  
 recognizer.pause_threshold = 0.8
 recognizer.dynamic_energy_threshold = False
 
