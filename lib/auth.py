a7888d508e039e9f85573958edd4edee48b3371e
Sree Narayanan <sreeaadhi07@gmail.com>
2024-04-20 21:51:33 +0530

    Update auth.py

--- lib/auth.py
+++ lib/auth.py
@@ -2,6 +2,12 @@ from lib.db import db
 from lib.custom_exception import CustomException
 
 async def check_user(user_id: str):
+    """
+    Check if a user exists in the database. Raise an exception if the user is not found.
+
+    Args:
+    - user_id (str): The user ID to check.
+    """
     user = await db.user.find_first(where={"id": user_id})
     if user is None:
         raise CustomException("User not found.", 401)
