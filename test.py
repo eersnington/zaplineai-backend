e52e668b9ce63574261a96a51916e4074b76d559
Sree Narayanan <sreeaadhi07@gmail.com>
2024-05-15 10:14:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
-from lib.llm_model import ClassifierModel
-from lib.llm_prompt import get_classifier_prompt
+from lib.llm_model import ClassifierModel, LLMModel
+from lib.llm_prompt import get_classifier_prompt, get_chat_prompt
+from lib.cached_response import get_example_response
 import time
 
-classifier = ClassifierModel()
 
-prompt = get_classifier_prompt("I want to return my order")
-start = time.time()
-response = classifier.classify(prompt)
-end = time.time()
-print(response)
-print(f"Time taken: {end - start} seconds")
-print(f"Throughput: {len(response) / (end - start)} tokens per second")
+classifier = ClassifierModel()
+llm = LLMModel()
+
+bot_name = "Sunny"
+store_name = "Sunshine Swimsuits"
+
+while True:
+    print("")
+    user_input = input("Enter your message: ")
+    if user_input == "exit":
+        break
+    
+    classifier_prompt = get_classifier_prompt(user_input)
+    start = time.time()
+    message_intent = classifier.classify(classifier_prompt)
+    end = time.time()
+    print(f"Message intent: {message_intent}")
+    print(f"Time taken for classification: {end - start} seconds")
+
+    example_response = get_example_response(message_intent)
+    print(f"Example response: {example_response}")
