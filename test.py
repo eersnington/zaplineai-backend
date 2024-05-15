ac4c432f18a8b644d8188374261d4bb182df9cee
Sree Narayanan <sreeaadhi07@gmail.com>
2024-05-15 09:50:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 from lib.llm_model import ClassifierModel
 from lib.llm_prompt import get_classifier_prompt
+import time
 
 classifier = ClassifierModel()
 
 prompt = get_classifier_prompt("I want to return my order")
+start = time.time()
 response = classifier.classify(prompt)
+end = time.time()
 print(response)
+print(f"Time taken: {end - start} seconds")
+print(f"Throughput: {len(response) / (end - start)} tokens per second")
