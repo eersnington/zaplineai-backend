33184064a7f7b9d50d23bda832a48184fb056e38
Sree Narayanan <sreeaadhi07@gmail.com>
2024-05-15 09:41:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,40 +1,8 @@
-from huggingface_hub import login
-from optimum.gptq import GPTQQuantizer
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from lib.llm_model import ClassifierModel
+from lib.llm_prompt import get_classifier_prompt
 
-login()
+classifier = ClassifierModel()
 
-dataset_id = "wikitext2"
-
-
-quantizer = GPTQQuantizer(bits=4, dataset=dataset_id, model_seqlen=2048)
-quantizer.quant_method = "gptq"
-
-
-
-model_id = "meta-llama/Meta-Llama-3-8B"
-
-model = AutoModelForCausalLM.from_pretrained(model_id, config=quantizer, torch_dtype=torch.float16, max_memory = {0: "15GIB", 1: "15GIB"})
-tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=False)
-
-
-import os
-import json
-
-quantized_model = quantizer.quantize_model(model, tokenizer)
-
-# save the quantized model
-save_folder = "quantized_llama"
-quantized_model.save_pretrained(save_folder, safe_serialization=True)
-
-# load fresh, fast tokenizer and save it to disk
-tokenizer = AutoTokenizer.from_pretrained(model_id).save_pretrained(save_folder)
-
-# save quantize_config.json for TGI
-with open(os.path.join(save_folder, "quantize_config.json"), "w", encoding="utf-8") as f:
-  quantizer.disable_exllama = False
-  json.dump(quantizer.to_dict(), f, indent=2)
-
-
-quantized_model.push_to_hub("Sreenington/Meta-llama-3-8B-GPTQ")
+prompt = get_classifier_prompt("I want to return my order")
+response = classifier.classify(prompt)
+print(response)
