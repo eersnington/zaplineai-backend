ebf50db23d14ae0ac9687572c95485c67d233b7c
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-26 14:40:52 +0400

    Update cached_response.py

--- lib/cached_response.py
+++ lib/cached_response.py
@@ -204,6 +204,8 @@ cached_intent_responses = {
     "Returns Step-2": "Thank you for sharing the reason for your return. I've started the return process for you, and someone from our team will be in touch soon to assist you further. Do you need any other help?",
     "Refund Step-2": "Thanks for letting us know why you're requesting a refund. I've initiated the refund process for you, and our team will reach out soon to assist you further. Do you need any other help?",
     "Cancellation Step-2": "Thanks for letting us know why you're requesting a cancellation. I've requested to cancel the order, and our team will reach out soon to assist you further. Do you need any other help?",
+            "Cancellation Step-2": "Ask them if they want a full refund or order something else (tell any similar product) in stock with 50$ discount",
+
 }
 
 cached_order_status_responses = {
