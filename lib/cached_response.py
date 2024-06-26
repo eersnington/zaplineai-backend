06f3be8ccdc63198d5480fafdd7fd166721543db
Sree Narayanan <sreeaadhi07@gmail.com>
2024-06-26 14:17:48 +0400

    Update cached_response.py

--- lib/cached_response.py
+++ lib/cached_response.py
@@ -227,7 +227,7 @@ def get_order_status_response(status):
 def get_example_response(intent):
     """Fetches an example response for a given intent."""
     cached_example_responses = {
-        "Order Status": "Say in this format- Your last order of (mention the items) on (mention date) is [explain the status]. Do you need any other help?",
+        "Order Status": "Say in this format- Your last order of (mention the items) on (mention date) is [explain the status]. It will arrive in [estimate the number of days].",
         "Cancellation Step-1": "Say this - I'm sorry to hear you want to cancel your order. Could you please tell me the reason for the cancellation?",
         "Cancellation Step-2": "Ask them if they want a full refund or order something else (tell any similar product) in stock with 50$ discount",
         "Cancellation Step-3": "Say this - Your cancellation request is being processed. Our support team will get back to you shortly.",
